f4c34fc4d324608f30c53238278bfb537eaff843
Jitendra Kumar Bijarnia <shivtej.img@gmail.com>
2024-04-01 10:13:01 +0530

    color code set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme1': '#f3890f ',
+        'theme2': '#1668e2',
+        'theme3': '#171347 ',
+      },
+    },
   },
   plugins: [],
 }
